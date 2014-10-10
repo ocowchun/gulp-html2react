8c0709d879461a6143563c0606da8e932e5d5f98
ocowchun <ocowchun@gmail.com>
2014-10-10 14:51:02 +0800

    New: add html2index

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+var path = require('path');
+var gutil = require('gulp-util');
+var through = require('through2');
+var react = require('react-tools');
+var reactDomPragma = require('react-dom-pragma');
+
+module.exports =  function(options) {
+	return through.obj(function(file, enc, cb) {
+		if (file.isNull()) {
+			cb(null, file);
+			return;
+		}
+
+		if (file.isStream()) {
+			cb(new gutil.PluginError('gulp-html2react', 'Streaming not supported'));
+			return;
+		}
+
+		var str = file.contents.toString();
+		var filePath = file.path;
+
+		if (path.extname(filePath) === '.html') {
+
+			str= " var React = require('react'); \n module.exports =function(){return ("+str +");};";
+			str = reactDomPragma(str);
+		}
+
+		try {
+			file.contents = new Buffer(react_tools.transform(str, options));
+			file.path = gutil.replaceExtension(filePath, '.js');
+			cb(null, file);
+		} catch (err) {
+			cb(new gutil.PluginError('gulp-react', err, {
+				fileName: filePath
+			}));
+		}
+	});
+};
